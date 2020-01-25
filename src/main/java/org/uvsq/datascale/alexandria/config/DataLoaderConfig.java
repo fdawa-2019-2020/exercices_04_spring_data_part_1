0f7aa9871f97c8301e48f287472df8e2db295e36
Marc DeXeT <marc.dexet@ias.u-psud.fr>
2020-01-25 18:51:09 +0100

    DataLoaderConfig with entityManager

--- src/main/java/org/uvsq/datascale/alexandria/config/DataLoaderConfig.java
+++ src/main/java/org/uvsq/datascale/alexandria/config/DataLoaderConfig.java
@@ -2,13 +2,14 @@ package org.uvsq.datascale.alexandria.config;
 
 import java.text.SimpleDateFormat;
 
+import javax.persistence.EntityManager;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.transaction.annotation.Transactional;
 import org.uvsq.datascale.alexandria.domain.Author;
 
@@ -18,7 +19,7 @@ public class DataLoaderConfig {
 	private static Logger logger = LoggerFactory.getLogger(DataLoaderConfig.class);
 	
 	@Bean
-	public ApplicationRunner authorLoader(JdbcTemplate template) {
+	public ApplicationRunner authorLoader(EntityManager entityManager) {
 		return new ApplicationRunner() {
 			SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
 			
@@ -30,18 +31,12 @@ public class DataLoaderConfig {
 				victor.setFirstname("Victor");
 				victor.setLastname("Hugo");
 				victor.setBirthDate(simpleDateFormat.parse("1802-02-26"));
-				saveAuthor(victor, template);
+				saveAuthor(victor, entityManager);
 			}
 		};
 	}
 
-	protected void saveAuthor(Author victor, JdbcTemplate template) {
-		template.update("INSERT INTO Author (id, firstname, lastname, birth_date) VALUES (?,?,?,?)", 
-				new Object[] {
-					new Long(1L),
-					victor.getFirstname(),
-					victor.getLastname(),
-					victor.getBirthDate()
-				}); 		
+	protected void saveAuthor(Author victor, EntityManager entityManager) {
+		entityManager.persist(victor);
 	}
 }
