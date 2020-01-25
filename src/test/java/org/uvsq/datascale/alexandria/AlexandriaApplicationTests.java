34ccd21d30f6c959b8bdc4c55dba386acc325d15
Marc DeXeT <marc.dexet@ias.u-psud.fr>
2020-01-25 15:56:05 +0100

    Initialized Spring boot project

---
+++ src/test/java/org/uvsq/datascale/alexandria/AlexandriaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.uvsq.datascale.alexandria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlexandriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
